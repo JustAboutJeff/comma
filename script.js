3ead8aa4956906b4e18d7db72cdf4b8c40658a98
JEFF <justaboutjeff@gmail.com>
2013-08-18 10:59:41 -0500

    punctuation regex feature in place

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ var CommaMachine = {
     $('p.content').on('click','span', this.toggleComma);
   },
   toggleComma: function() {
-    $(this).toggleClass('comma');
+    $this = $(this);
+    var strippedWord = $this.text().replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    $this.text(strippedWord).toggleClass('comma');
   }
 };
 
